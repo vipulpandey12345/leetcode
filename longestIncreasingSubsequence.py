0a373c8f6e0971dda1fc695c03bd5b56dbe9eaa7
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2022-02-23 03:14:02 -0500

    Update longestIncreasingSubsequence.py

--- longestIncreasingSubsequence.py
+++ longestIncreasingSubsequence.py
@@ -1,1 +1,10 @@
-
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        set_of_numbers_so_far = set()
+        dp_arr = []
+        for i in nums:
+  #keep adding on
