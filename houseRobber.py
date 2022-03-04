4479c344d053db769d343d6515b378c3da8cd074
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2022-03-04 16:55:02 -0500

    Create houseRobber.py

---
+++ houseRobber.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rob(self, arr):
+        if len(arr) == 0:
+            return 0
+        dp = [0] * len(arr)
+        for i in range(len(arr)):
+            if i == 0:
+                dp[i] = arr[i]
+            elif i == 1:
+                dp[i] = max(dp[i-1], arr[i])
+            else:
+                dp[i] = max(dp[i-2] + arr[i], dp[i-1])
+        return max(dp)
+        
