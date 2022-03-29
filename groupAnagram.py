b6c8e52b9b7169eed770412f5e2a476cd3f6c0d8
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2022-03-29 14:05:35 -0400

    Update groupAnagram.py

--- groupAnagram.py
+++ groupAnagram.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groupings = {}
+        for i in strs:
+            current_word = [0] * 26
+            for j in i:
+                letter_index = ord(j) - ord('a')
+                current_word[letter_index] += 1
+            current_word = tuple(current_word)
+            if current_word in groupings:
+                groupings[current_word].append(i)
+            else:
+                groupings[current_word] = [i]
+        return groupings.values()
