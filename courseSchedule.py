02ceb62625a2c6b1f784e17530ecd1f41ee95770
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2022-05-15 16:07:41 -0700

    Update courseSchedule.py

--- courseSchedule.py
+++ courseSchedule.py
@@ -1,20 +1,27 @@
 from collections import defaultdict
-def numCourses(prerequisites, numCourses):
-  connections = defaultdict(list) #storing a node and all outgoing connections
-  numCourseConnections = defaultdict(int)#storing a counter for the number of connections
-  for course, prereq in range(len(prerequisites)):
-    connections[prereq].append(course)
-    numCourseConnections[course] += 1
-  queue = []
-  for key in numCourseConnections.keys():
-    if numCourseConnections[key] == 0:
-      queue.append(key)
-  visited = set()
-  while len(queue) != 0:
-    first_position = queue.pop()
-    visited.add(first_position)
-    for neighbor in connections[first_position]:
-      numCourseConnections[j] -= 1
-      if numCourseConnections[j] == 0 and neighbor not in visited:
-        queue.append(j)
-  return len(visited) == NumCourses
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        connections = defaultdict(list) #storing a node and all outgoing connections
+        numCourseConnections = defaultdict(int)
+        visited = set()
+        queue = []
+        for i in range(0,numCourses):
+            numCourseConnections[i] = 0
+            
+        for course,prereq in prerequisites:
+            connections[prereq].append(course)
+            numCourseConnections[course] += 1
+        for key in numCourseConnections.keys():
+            if numCourseConnections[key] == 0:
+                queue.append(key)
+        while len(queue) != 0:
+            current_course = queue.pop()
+            if current_course in visited:
+                continue
+            else:
+                visited.add(current_course)
+            for neighbor in connections[current_course]:
+                numCourseConnections[neighbor] -= 1
+                if numCourseConnections[neighbor] == 0:
+                    queue.append(neighbor)
+        return len(visited) == numCourses
