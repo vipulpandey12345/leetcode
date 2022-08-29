1ef622e9cb65decb4dcf44e697381b481e540cda
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2022-08-29 13:11:30 -0400

    Create insertIntoBST.py

---
+++ insertIntoBST.py
@@ -0,0 +1,18 @@
+"""
+- if the value is less, then move to the leftside
+- if the value is more, then move to the rightside
+- if the root becomes none, then check whether the left 
+
+"""
+
+
+
+
+
+def insertIntoBST(node):
+  
+
+  def helper(dummyNode):
+    if node == dummyNode:
+      return -1
+
