d915edf3d1bac7bbe436fd1b75ef5cd5729e25a2
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2022-10-26 19:09:12 -0400

    Update minimumInSortedArr.py

--- minimumInSortedArr.py
+++ minimumInSortedArr.py
@@ -4,10 +4,4 @@ class Solution:
         left, right = 0, len(nums) - 1
         res = -1
         while left < right:
-            mid = (left + right) // 2
-            if nums[mid] > nums[right]:
-                left = mid + 1
-            else:
-                right = mid
-        
-        return nums[left]
+          
