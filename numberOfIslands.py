285c3f941c3f999a47ae9fd826e33de6fe138baf
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2025-07-16 01:07:32 -0700

    Update numberOfIslands.py

--- numberOfIslands.py
+++ numberOfIslands.py
@@ -1,31 +1,24 @@
-def get_number_of_islands(binaryMatrix):
-  if len(binaryMatrix) == 0:
-    return 0
-  length = len(binaryMatrix)
-  height = len(binaryMatrix[0])
-  if length == 0 or height == 0:
-    return 0
-  num = 0
-  visited = set()
-  """
-  visitied = [[False] * height for _ range(length)]
-  """
-  for i in range(length):
-    for j in range(height):
-      if binaryMatrix[i][j] == 1 and (i,j) not in visited:
-        num += 1
-        curr_position = (i,j)
-        helper(visited,binaryMatrix,curr_position,length,height)
-  return num
-
-def helper(visited,binaryMatrix,curr_position,length,height):
-  if curr_position in visited:
-    return
-  visited.add(curr_position)
-  for x1, y1 in [(0,1),(1,0),(-1,0),(0,-1)]:
-    x, y = curr_position# (0,1) => x = 0, y = 1
-    new_x, new_y = x + x1, y + y1
-    if 0 <= new_x < length and 0 <= new_y < height:
-      new_position = binaryMatrix[new_x][new_y]
-      if new_position == 1:
-        helper(visited,binaryMatrix,(new_x,new_y),length,height)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(i,j):
+            if (i,j) in visited:
+                return
+            if grid[i][j] == '0':
+                return
+            visited.add((i,j))
+            moves = [[1,0],[-1,0],[0,1],[0,-1]]
+            for x, y in moves:
+                if 0 <= x + i < length:
+                    if 0 <= y + j < width:
+                        dfs(x + i, y + j)
+        length = len(grid)
+        width = len(grid[0])
+        visited = set()
+        number_of_islands = 0
+        for i in range(length):
+            for j in range(width):
+                if grid[i][j] == '1' and (i,j) not in visited:
+                    number_of_islands += 1
+                    dfs(i,j)
+        return number_of_islands
+        
